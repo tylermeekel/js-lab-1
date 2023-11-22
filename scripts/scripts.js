00584432e1f30fcb549c544f5ae7dd105cdc60e1
Tyler Meekel <84874612+tylermeekel@users.noreply.github.com>
2023-11-22 09:42:44 -0700

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,44 @@
+/****************** create variables ******************/
+let modelName = "XYZ"
+let duration = 1
+
+/****************** helper function ******************/
+function recalculate() {
+    let costLabel = document.getElementById("calculated-cost");
+
+    if (modelName === "XYZ") {
+        costLabel.innerHTML = duration*100
+    } else if (modelName === "CPRG"){
+        costLabel.innerHTML = duration*213
+    }
+}
+
+/****************** model button logic ******************/
+let modelButton = document.getElementById("model-button");
+console.log(modelButton)
+
+function changeModel() {
+    modelText = document.getElementById('model-text')
+
+    if (modelName === "XYZ") {
+        modelName = "CPRG"
+    } else if (modelName === "CPRG"){
+        modelName = "XYZ"
+    }
+    modelText.innerHTML = "Model " + modelName
+    recalculate()
+}
+modelButton.addEventListener("click", changeModel);
+
+/****************** duration button logic ******************/
+let durationButton = document.getElementById("duration-button")
+
+function changeDuration() {
+    let durationText = document.getElementById("duration-text")
+    duration = prompt("enter new duration:")
+    
+    durationText.innerHTML = duration
+    recalculate()
+}
+
+durationButton.addEventListener("click", changeDuration)
